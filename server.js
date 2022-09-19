ae4bf78ae24dd552dfc4bb0dfa163d73ed2252db
agupta <agupta@gsmarketing.com>
2022-09-19 17:44:00 +0530

    update

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use("/", (req, res, next) => {
   try {
     console.log(req.path);
-    if (req.path == "/login" || req.path == "/register" || req.path == "/" || req.path == "/contact") {
+    if (req.path == "/login" || req.path == "/register" || req.path == "/") {
       next();
     } else {
       /* decode jwt token if authorized*/
