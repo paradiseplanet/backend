086145fa882ed9cda7ff12a8afad537edd651aff
agupta <agupta@gsmarketing.com>
2022-09-19 11:12:08 +0530

    update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
-var multer = require('multer'),
-bodyParser = require('body-parser'),
+var multer = require('multer');
+bodyParser = require('body-parser');
 path = require('path');
 var mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://paradiseplanet:100Mic10*@paradiseplanet.ev1vovz.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true});
